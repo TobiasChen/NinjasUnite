2b4c37cd3ccab6ff1307731c9c80dd129269db66
Tobias Chen <tobias@chen.de>
2017-04-10 11:48:41 +0200

    Level now correctly load and unload depending on Camera distance.Level now only have to be placed inside the resource folder

--- Assets/_Scripts/LevelSpawn.cs
+++ Assets/_Scripts/LevelSpawn.cs
@@ -1,64 +1,81 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Linq;
 using UnityEngine;
-
-public class LevelSpawn : MonoBehaviour {
-
-    public GameObject Startlevel; //Probably going to be replaced by the Array 
-    public GameObject DeathTrigger;
-    public Camera Cam;
-    public GameObject[] Level;
-    public List<GameObject> ActiveLevel;
-    private float nextPosition;
-    private GameObject CurrentLevel;
-    private GameObject NextLevel;
-    private int temp;  //Temp Value for testing
-    public float Gap; // Distance between levels
-
-
-	void Start ()
+using System.Collections;
+using System;
+using System.Collections.Generic;
+public class LevelSpawn : MonoBehaviour
     {
-        Cam = Camera.main;
-        temp = 0;
-        nextPosition = 0;
-        NextLevel = Startlevel;
-    }
+
+        public GameObject Startlevel;   //Probably going to be replaced by the Array 
+        public float Gap;               // Distance between levels
+        public float LoadDistance;      //When to Load the next Level (Default 200)
+        public float UnLoadDistance;    //When to UnLoad the last Level(Default 200)
+        
+        
+        private GameObject DeathTrigger;
+        private GameObject CurrentLevel;
+        private GameObject NextLevel;
+        
+        public List<GameObject> ActiveLevel;       //The List of currently active Level
+        public List<GameObject> ActiveDeathTrigger;//The List of currently active DeathTrigger   
+        private GameObject[] Level;                 //An Array which holds all the Level Prefabs.
+        private Camera Cam;                         //The Camera tracking the Player to calculated when to load or unload the level
+        private float nextPosition;                 //The Position of the next Spawned Level
+
+
+    void Start ()
+        {
+            Cam = Camera.main;                      // Camera gets initialized
+            nextPosition = 0;                       // Start Position is x =0
+            NextLevel = Startlevel;                 //The Game starts at the predefined Start Level
+
+            DeathTrigger = Resources.FindObjectsOfTypeAll(typeof(GameObject)).Cast<GameObject>().Where(g => g.name == "DeathTrigger").ToList()[0]; 
+            //Loads all Item Preafbs with the Name "DeathTrigger" into a List and returns the first Entry
+            //Not a very clean aproach, but gets the job done
+            Level = Resources.LoadAll<GameObject>("LevelPrefabs").Cast<GameObject>().ToArray(); 
+            //Loads all Items of Type Prefab in LevelPrefabs and casts them into an Array
+        }
 	
 	void Update ()
-    {
-        //  if (Cam.transform.position.x == nextPosition + 50)
-        //      Destroy();
-        if (temp < 20) //Choses wherever or not to spawn an additional levle
-            Spawn();
-        if (temp < 15 && temp > 3)
-            Destroy();
+        {
 
-    }
-    GameObject SelectLevel() // Goes over the Array selects a random level to spawn
-    {
-        int temp = Random.Range(0, Level.Length);
-        //return Level[temp];
-        return Startlevel;
-    }
-    private void Destroy()
-    {
-        GameObject ToDestroy;
-        ToDestroy = ActiveLevel[0];
-        ActiveLevel.Remove(ToDestroy);
-        print("Test");
-    }
+            if (Cam.transform.position.x >= nextPosition - LoadDistance)                                          //If the Camera is 200 units form the next Position away the next Level spawns
+                { 
+                    Spawn();
+                }
+            if (Cam.transform.position.x >= ActiveLevel[0].GetComponent<Transform>().position.x + UnLoadDistance) //If the Camera is further then 200 Units from the first Spawned Level away, the level despanws
+                {
+                    Destroy();
+                }
+        }
     void Spawn()
-    {
-        GameObject Death;
-        CurrentLevel = NextLevel;
-        NextLevel = SelectLevel(); //Selects next Level
-        Instantiate(CurrentLevel, new Vector2 (nextPosition, 0), Quaternion.identity);//Instantinates the level at the next Position, next Position is calculated the loop before.
-        ActiveLevel.Add(CurrentLevel);
-        //Death = Instantiate(DeathTrigger, new Vector2(0,-80), Quaternion.identity, NextLevel.transform);
-        Death = Instantiate(DeathTrigger, new Vector2(nextPosition+ (CurrentLevel.GetComponent<Renderer>().bounds.size.x / 2)+(Gap/2), -55), Quaternion.identity);//Instantiates a Deathtrigger between the last
-        Death.transform.localScale = new Vector3(Gap, 2, 1);
-        nextPosition += ((CurrentLevel.GetComponent<Renderer>().bounds.size.x/2)+(NextLevel.GetComponent<Renderer>().bounds.size.x/2)+Gap); //Calcutlates next Positiona as size of last level+last Position+ Gap.
+        {
+            GameObject Death;           //Local Variable Death
 
-        temp += 1; //temp value
+            CurrentLevel = NextLevel;   //Selects current Level
+            NextLevel = SelectLevel();  //Selects next Level
+            ActiveLevel.Add(Instantiate(CurrentLevel, new Vector2(nextPosition, 0), Quaternion.identity));
+            //Instantinates the level at the next Position, next Position is calculated the loop before.
+
+            Death = Instantiate(DeathTrigger, new Vector2(nextPosition + (CurrentLevel.GetComponent<Renderer>().bounds.size.x / 2) + (Gap / 2), -55), Quaternion.identity);
+            //Instantiates a Deathtrigger between the Current Level over the Gap
+            Death.transform.localScale = new Vector3(Gap, 2, 1); //Sets the Death Trigger to the correct Size
+            ActiveDeathTrigger.Add(Death); //Adds the newly Instantiated Trigger to the List
+            
+            nextPosition += ((CurrentLevel.GetComponent<Renderer>().bounds.size.x / 2) + (NextLevel.GetComponent<Renderer>().bounds.size.x / 2) + Gap);
+            //Calculates next Position as each half the size of last level and Next Level + Gap.
+        }
+    void Destroy()
+        {
+            UnityEngine.Object.Destroy(ActiveLevel[0]);         //Unloads the clone of the level from the Scene
+            UnityEngine.Object.Destroy(ActiveDeathTrigger[0]);  //Unloads the clone of the DeathTrigger from the Scene
+            ActiveLevel.Remove(ActiveLevel[0]);                 //Delets The now empty List Entry
+            ActiveDeathTrigger.Remove(ActiveDeathTrigger[0]);   //Delets the now empty List Entry
+        }
+    
+    GameObject SelectLevel() //Goes over the Array to select a random level to spawn
+        {
+            return Level[UnityEngine.Random.Range(0, Level.Length)];
+        }
+
     }
-}
